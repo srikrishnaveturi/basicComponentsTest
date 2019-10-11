71790fa4a818f489dfa9a18e7de26dd54af77240
Srikrishna Veturi <srikrishna.veturi18@siesgst.ac.in>
2019-10-11 23:25:44 +0530

    Update irTest.ino

--- irTest.ino
+++ irTest.ino
@@ -4,15 +4,16 @@ void setup()
 {
   // put your setup code here, to run once:
 Serial.begin(9600);
-pinMode(13,OUTPUT);
+pinMode(LED_BUILTIN,OUTPUT);
 }
-
+/* connect the postive terminal of the transmitter to the pwm pins that are connected to the clock of the microcontroller
+for arduino uno, connect to digital pin 3, for arduino mega, it is pin 9..*/
 void loop() {
   // put your main code here, to run repeatedly:
-irsend.sendNEC(0xDDDDDDD2,32);
-Serial.println("bhej dia");
-digitalWrite(13,HIGH);
+irsend.sendNEC(0x12345678,32);
+Serial.println("CODE SENT");
+digitalWrite(LED_BUILTIN,HIGH);
 delay(100);
-digitalWrite(13,LOW);
+digitalWrite(LED_BUILTIN,LOW);
 delay(100);
 }
